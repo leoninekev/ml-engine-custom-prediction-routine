23e9d38a9f1acdef46a9b294fb8cb6f0170fc947
Prakash Bisht <mightykevin007@gmail.com>
2019-05-30 14:50:57 +0530

    added pre-processing methods to class, added modules roi_helpers.py, RoiPoolingConv.py, FixdBacthNorm.py and Setup.py file to package code for creating version resource in GCP

--- predictor.py
+++ predictor.py
@@ -1,14 +1,65 @@
 import os
 import pickle
 
+from keras import backend as K#140519
+import cv2#140519
 
+#import sys
+from keras.layers import Input
+from keras.models import Model
+
+import roi_helpers#160519 #from keras_frcnn import roi_helpers
+import numpy as np
+import tensorflow as tf
 
 class Predictor(object):
     """Interface for constructing custom predictors."""
-    def __init__(self,config):#170519 model, preprocessor):
+    def __init__(self,config):
         self._config = config#loads the config file instance generated after training
 
-        
+    def format_img_size(self, img):#, C):#140519
+        """ formats the image size based on config """
+        img_min_side = float(self._config.im_size)
+        (height,width,_) = img.shape
+        if width <= height:
+            ratio = img_min_side/width
+            new_height = int(ratio * height)
+            new_width = int(img_min_side)
+        else:
+            ratio = img_min_side/height
+            new_width = int(ratio * width)
+            new_height = int(img_min_side)
+
+        img = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_CUBIC)
+        return img, ratio
+
+    def format_img_channels(self, img):#, C):#140519
+        """ formats the image channels based on config """
+        img = img[:, :, (2, 1, 0)]
+        img = img.astype(np.float32)
+        img[:, :, 0] -= self._config.img_channel_mean[0]
+        img[:, :, 1] -= self._config.img_channel_mean[1]
+        img[:, :, 2] -= self._config.img_channel_mean[2]
+        img /= self._config.img_scaling_factor
+        img = np.transpose(img, (2, 0, 1))
+        img = np.expand_dims(img, axis=0)
+        return img
+
+    def format_img(self, img):#, C): #140519
+        """ formats an image for model prediction based on config """
+        img, ratio = format_img_size(img)
+        img = format_img_channels(img)
+        return img, ratio
+    
+    def get_real_coordinates(ratio, x1, y1, x2, y2):#140519
+        real_x1 = int(round(x1 // ratio))
+        real_y1 = int(round(y1 // ratio))
+        real_x2 = int(round(x2 // ratio))
+        real_y2 = int(round(y2 // ratio))
+
+        return (real_x1, real_y1, real_x2 ,real_y2)      
+
+
 
     def predict(self, instances):#, **kwargs):
         """Performs custom prediction.
@@ -52,7 +103,8 @@ class Predictor(object):
         else:
             input_shape_img = (None, None, 3)
             input_shape_features = (None, None, num_features)
-        ####this all could be moved to classmethod below
+
+        ####this all could be moved to classmethod below later
         img_input = Input(shape=input_shape_img)
         roi_input = Input(shape=(self._config.num_rois, 4))
         feature_map_input = Input(shape=input_shape_features)
@@ -67,17 +119,82 @@ class Predictor(object):
         classifier = nn.classifier(feature_map_input, roi_input, self._config.num_rois, nb_classes=len(class_mapping), trainable=True)
 
         model_rpn = Model(img_input, rpn_layers)
-        model_classifier_only = Model([feature_map_input, roi_input], classifier)
 
         model_classifier = Model([feature_map_input, roi_input], classifier)
-        #print('Loading weights from {}'.format(C.model_path))
-
-        model_rpn.load_weights(self._config.model_path, by_name=True)
+    
         #150519 #explicit model_path could also be given to folder actually holding weights
+        model_rpn.load_weights(self._config.model_path, by_name=True)#model_path is './model_frcnn.hdf5'
         model_classifier.load_weights(self._config.model_path, by_name=True)
     
         model_rpn.compile(optimizer='sgd', loss='mse')
         model_classifier.compile(optimizer='sgd', loss='mse')
+
+        all_imgs = []
+        classes = {}
+        
+
+        inputs = np.asarray(instances)#receives input in byte string format from image loaded from gcloud storage
+
+        X, ratio = self.format_img(inputs)#220519#, C)#140519
+        if K.image_dim_ordering() == 'tf':
+            X = np.transpose(X, (0, 2, 3, 1))
+
+        [Y1, Y2, F] = model_rpn.predict(X)
+        R = roi_helpers.rpn_to_roi(Y1, Y2, self._config, K.image_dim_ordering(), overlap_thresh=0.7)
+        R[:, 2] -= R[:, 0]
+        R[:, 3] -= R[:, 1]
+        bboxes = {}
+        probs = {}
+        bbox_threshold = 0.8
+
+        for jk in range(R.shape[0]//C.num_rois + 1):
+            ROIs = np.expand_dims(R[self._config.num_rois*jk:self._config.num_rois*(jk+1), :], axis=0)
+            if ROIs.shape[1] == 0:
+                break
+            if jk == R.shape[0]//self._config.num_rois:
+                #pad R
+                curr_shape = ROIs.shape
+                target_shape = (curr_shape[0],self._config.num_rois,curr_shape[2])
+                ROIs_padded = np.zeros(target_shape).astype(ROIs.dtype)
+                ROIs_padded[:, :curr_shape[1], :] = ROIs
+                ROIs_padded[0, curr_shape[1]:, :] = ROIs[0, 0, :]
+                ROIs = ROIs_padded
+            [P_cls, P_regr] = model_classifier_only.predict([F, ROIs])
+            for ii in range(P_cls.shape[1]):
+                if np.max(P_cls[0, ii, :]) < bbox_threshold or np.argmax(P_cls[0, ii, :]) == (P_cls.shape[2] - 1):
+                    continue
+                cls_name = class_mapping[np.argmax(P_cls[0, ii, :])]
+                
+                if cls_name not in bboxes:
+                    bboxes[cls_name] = []
+                    probs[cls_name] = []
+                (x, y, w, h) = ROIs[0, ii, :]
+                cls_num = np.argmax(P_cls[0, ii, :])
+                try:
+                    (tx, ty, tw, th) = P_regr[0, ii, 4*cls_num:4*(cls_num+1)]
+                    tx /= self._config.classifier_regr_std[0]
+                    ty /= self._config.classifier_regr_std[1]
+                    tw /= self._config.classifier_regr_std[2]
+                    th /= self._config.classifier_regr_std[3]
+                    x, y, w, h = roi_helpers.apply_regr(x, y, w, h, tx, ty, tw, th)
+                except:
+                    pass
+                bboxes[cls_name].append([self._config.rpn_stride*x, self._config.rpn_stride*y, self._config.rpn_stride*(x+w), self._config.rpn_stride*(y+h)])
+                probs[cls_name].append(np.max(P_cls[0, ii, :]))
+        all_dets = []
+
+        for key in bboxes:
+            bbox = np.array(bboxes[key])
+            new_boxes, new_probs = roi_helpers.non_max_suppression_fast(bbox, np.array(probs[key]), overlap_thresh=0.5)
+
+            for jk in range(new_boxes.shape[0]):
+                (x1, y1, x2, y2) = new_boxes[jk,:]
+                #(real_x1, real_y1, real_x2, real_y2) = get_real_coordinates(ratio, x1, y1, x2, y2)
+                coord_list= list(self.get_real_coordinates(ratio, x1, y1, x2, y2))#220519#
+                #print('norm coord:',real_x1, real_y1, real_x2, real_y2, '\n \n coord_list',coord_list)
+                all_dets.append((key,100*new_probs[jk],coord_list))
+        return all_dets
+    
         #raise NotImplementedError()
     
 
@@ -98,6 +215,8 @@ class Predictor(object):
         config_path = os.path.join(model_dir, 'config.pickle')#config file instance of class holding key parameters
         with open(config_path, 'rb') as f_in:#140519# load config file from local path here
             config = pickle.load(f_in)
+            #NOTE:data unpickled here to config is an instance of Class Config in file trainer.config.Config, of whihc sub variables inside can
+            #be accessed in a way much like a method using config.xyz form.
 
         return cls(config)
         #raise NotImplementedError()
